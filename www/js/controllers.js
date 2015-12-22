c25f09eefe6a6d2df07bd54f81e828f902399d64
Steven Kessler <stvnksslr@gmail.com>
2015-12-22 16:40:54 -0500

    feature(barcode): adding barcode scanner to application

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,7 +2,22 @@ angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope) {})
 
-.controller('BarcodeCtrl', function($scope) {})
+.controller("BarcodeCtrl", function($scope, $cordovaBarcodeScanner) {
+
+  console.log('ExampleController got called');
+  $scope.scanBarcode = function() {
+    console.log('scanBarcode function got called');
+    $cordovaBarcodeScanner.scan().then(function(imageData) {
+      console.log(imageData);
+      alert(imageData.text);
+      console.log("Barcode Format -> " + imageData.format);
+      console.log("Cancelled -> " + imageData.cancelled);
+    }, function(error) {
+      console.log("An error happened -> " + error);
+    });
+  };
+    
+})
 
 .controller('AccountCtrl', function($scope) {
     $scope.settings = {
